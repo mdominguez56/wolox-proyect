b535a413b1c5e58e60b095083b38b0337956081c
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-11-11 00:45:31 -0300

    linter-configuration

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "plugin:react/recommended",
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "ecmaFeatures": {
+        "experimentalObjectRestSpread": true
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}}
