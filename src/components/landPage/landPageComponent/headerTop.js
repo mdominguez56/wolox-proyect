88e046027c257bb60a75a19b14c795f6f2511199
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-11-12 01:54:11 -0300

    land-page-header-top

--- src/components/landPage/landPageComponent/headerTop.js
+++ src/components/landPage/landPageComponent/headerTop.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function HeaderTop() {
     return (
-        <div>Header Top in Land Page</div>
-        );
-  }
+        <div>
+          <img src="../../../assets/assets/logo_full_color.svg" alt="wolox-logo" />
+          <nav>
+            <Link to="/">Inicio</Link>
+            <Link to="/">Beneficios</Link>
+            <Link to="/">Login</Link>
+          </nav>
+        </div>
+        
+  );
+}
 
 export default HeaderTop
