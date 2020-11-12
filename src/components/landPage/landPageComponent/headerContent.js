d4e38aaeed61d3cef8692f9c59ae812514f41140
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-11-12 10:01:43 -0300

    header-top-land-page

--- src/components/landPage/landPageComponent/headerContent.js
+++ src/components/landPage/landPageComponent/headerContent.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './headerContent.module.css'
+import HeroImage from '../../../assets/Img Hero/Ic_ilustra_Hero.png'
 
 function HeaderContent() {
     return (
         <div className={styles.headerContentConteiner}>
           <h2>Bienvenido a tu entrevista tecnica en WOLOX</h2>
-          <img src="../../../assets/assets/ImgHero/Ic_ilustra_Hero.png" alt="Header-Image" />
+          <img src={HeroImage} alt="hero-image"/>
         </div>
         );
   }
--- src/components/landPage/landPageComponent/headerTop.js
+++ src/components/landPage/landPageComponent/headerTop.js
@@ -6,18 +6,18 @@ import {
   Link
 } from "react-router-dom";
 import styles from './headerTop.module.css'
+import logo from '../../../assets/logo_full_color.svg'
 
 function HeaderTop() {
     return (
         <div className={styles.container}>
-          <img src='' alt="" />
-          <nav>
+          <img src={logo} alt="wolox-logo" />
+          <nav className={styles.navLink}>
             <Link to="/">Inicio</Link>
             <Link to="/">Beneficios</Link>
             <Link to="/">Login</Link>
           </nav>
         </div>
-        
   );
 }
 
