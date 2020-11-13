ff59ba60fedfd15e322eca762e2c65696f0a0b36
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-11-13 10:01:22 -0300

    sites-in-development

--- src/components/listPage/listPage.js
+++ src/components/listPage/listPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import styles from '../landPage/landPageComponent/headerContent.module.css'
+import HeroImage from '../../assets/Img Hero/Ic_ilustra_Hero.png'
 
 function ListPage() {
-    return (
-      <div>
-          <p>Sitio en construccion</p>
+  return (
+    <div className={styles.headerContentConteiner}>
+      <div className={styles.center}>
+        <p className={styles.headerContentText}>Formulario de registro <br/> en <span className={styles.fontGreen}>desarrollo</span></p>
       </div>
+      <div className={styles.center}>
+        <img src={HeroImage} alt="hero-image" className={HeroImage}/>
+      </div>
+    </div>
     );
   }
   
--- src/components/registerPage/registerPage.js
+++ src/components/registerPage/registerPage.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import styles from '../landPage/landPageComponent/headerContent.module.css'
+import HeroImage from '../../assets/Img Hero/Ic_ilustra_Hero.png'
 
 function RegisterPage() {
-    return (
-      <div>
-          <p>Sitio en construccion - Register</p>
+  return (
+    <div className={styles.headerContentConteiner}>
+      <div className={styles.center}>
+        <p className={styles.headerContentText}>Listado de aplicaciones <br/> en <span className={styles.fontGreen}>desarrollo</span></p>
       </div>
+      <div className={styles.center}>
+        <img src={HeroImage} alt="hero-image" className={HeroImage}/>
+      </div>
+    </div>
     );
   }
   
