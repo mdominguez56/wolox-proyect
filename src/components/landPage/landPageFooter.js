7acab3cced0475d0c2566cec804055fcbfe70b9f
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-11-12 22:10:02 -0300

    land-page-footer

--- src/components/landPage/landPageFooter.js
+++ src/components/landPage/landPageFooter.js
@@ -6,11 +6,22 @@ import {
     Link
   } from "react-router-dom";
   import styles from './landPageFooter.module.css'
+  import WoloxLogo from '../../assets/Ic_Wolox_Footer.svg'
 
 
   function LandPageFooter() {
     return (
-        <div className={styles.backgroundFooter}>LAND PAGE FOOTER</div>
+        <div className={styles.backgroundFooter}>
+          <div>
+            <h3>Gracias por <span className={styles.wordLightBlue}>completar el ejercicio</span></h3>
+            <h4>Te invitamos a ver mas informacion</h4>
+            <button className={styles.button}>Conocer mas</button>
+          </div>
+          <div>
+            <img src={WoloxLogo} alt="wolox-logo" />
+          </div>
+          
+        </div>
         );
   }
 
