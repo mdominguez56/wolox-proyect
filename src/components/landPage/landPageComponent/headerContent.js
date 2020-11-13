abb440f95b1cfb232979aa6be85b3a8f5db75690
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-11-12 23:56:09 -0300

    modified-header-content

--- src/components/landPage/landPageComponent/headerContent.js
+++ src/components/landPage/landPageComponent/headerContent.js
@@ -6,8 +6,12 @@ import HeroImage from '../../../assets/Img Hero/Ic_ilustra_Hero.png'
 function HeaderContent() {
     return (
         <div className={styles.headerContentConteiner}>
-          <h2 className={styles.headerContentText}>Bienvenido a tu <br/> entrevista tecnica en <br/> WOLOX</h2>
-          <img src={HeroImage} alt="hero-image" className={HeroImage}/>
+          <div className={styles.center}>
+            <p className={styles.headerContentText}>Bienvenido a tu <br/><span className={styles.fontBold}>entrevista tecnica en</span><br/> <span className={styles.fontGreen}>WOLOX</span></p>
+          </div>
+          <div className={styles.center}>
+            <img src={HeroImage} alt="hero-image" className={HeroImage}/>
+          </div>
         </div>
         );
   }
