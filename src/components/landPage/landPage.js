67a04980161f0e64d8499bdc3e675b69fe6ff310
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-11-13 07:43:58 -0300

    create-animation-landpage

--- src/components/landPage/landPage.js
+++ src/components/landPage/landPage.js
@@ -5,6 +5,7 @@ import {
     Route,
     Link
   } from "react-router-dom";
+import {Spring} from 'react-spring/renderprops'
 import LandPageFooter from './landPageFooter';
 import LandPageHeader from './landPageHeader';
 import LandPageMain from './landPageMain'
@@ -13,11 +14,21 @@ import styles from './landPage.module.css'
 function landPage() {
     return (
     <Router>
-        <div className={styles.container}>
-            <LandPageHeader />
-            <LandPageMain />
-            <LandPageFooter />
-        </div>
+        <Spring
+        from={{ opacity: 0, marginTop: -500}}
+        to={{ opacity: 1, marginTop: 0}}
+        >
+            { props => (
+                <div style={props}>
+                    <div className={styles.container}>
+                        <LandPageHeader />
+                        <LandPageMain />
+                        <LandPageFooter />
+                     </div>
+                </div>
+                ) }
+        </Spring>
+        
     </Router>
     );
 }
